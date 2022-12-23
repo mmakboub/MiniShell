7a8682c064a3a3ca7a74de9d67a4990150f8225e
Mariam Makboub <mmakboub@e1r6p6.1337.ma>
2022-12-23 20:52:38 +0100

    still handling pipe

--- exect/pipe.c
+++ exect/pipe.c
@@ -6,8 +6,54 @@
 /*   By: mmakboub <mmakboub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 18:54:30 by mmakboub          #+#    #+#             */
-/*   Updated: 2022/12/22 21:07:14 by mmakboub         ###   ########.fr       */
+/*   Updated: 2022/12/23 20:42:36 by mmakboub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"minishell.h"
+
+void handle_pipe(char *path, char **env, char **argv)
+{
+    int fd[2];
+    int i = 0;
+    t_command *node;
+
+    int argc = 2;
+    int pip = argc - 1;
+    int in_tmp = dup(0);
+    while (node != NULL)
+    {
+        path , argv , env;
+        if(node->next)
+            pipe(fd);
+        pid_t pid = fork();
+        if (pid = 0)
+        {
+            if (node->next) {    
+                close(1);
+                dup2(fd[1],1);
+                close(fd[1]);
+                close(fd[0]);
+            }
+            if(execve(path, argv, env) == -1);
+                exit(1);
+        }
+        close(0);
+        if(node->next)
+        {    
+            dup2(fd[0],0);
+            close(fd[1]);
+            close(fd[0]);
+        }
+        i++;
+        node = node->next;
+    }
+    dup2(in_tmp, 0);
+    close(in_tmp);
+    i = 0;
+    while (i < argc)
+    {
+        wait(NULL);
+        i++;
+    }
+}
--- main.c
+++ main.c
@@ -6,56 +6,9 @@
 /*   By: mmakboub <mmakboub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 17:34:13 by mmakboub          #+#    #+#             */
-/*   Updated: 2022/12/23 20:19:02 by mmakboub         ###   ########.fr       */
+/*   Updated: 2022/12/23 20:36:54 by mmakboub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-int main(int ac, char **av, char **env)
-{
-    int fd[2];
-    int i = 0;
-    t_element *element;
-
-    int process = 2;
-    int pip = process - 1;
-    int in_tmp = dup(0);
-    while (element != NULL)
-    {
-        path , argv , env;
-        if(element->next)
-            pipe(fd);
-        pid_t pid = fork();
-        if (pid = 0)
-        {
-            if (element->next) {    
-                close(1);
-                dup2(fd[1],1);
-                close(fd[1]);
-                close(fd[0]);
-            }
-            if(execve(path, cmd, env) == -1);
-                exit(1);
-        }
-        close(0);
-        if(element->next)
-        {    
-            dup2(fd[0],0);
-            close(fd[1]);
-            close(fd[0]);
-        }
-        i++;
-        element = element->next;
-    }
-    dup2(in_tmp, 0);
-    close(in_tmp);
-    i = 0;
-    while (i < process)
-    {
-        wait(NULL);
-        i++;
-    }
+#
 }
